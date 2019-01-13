302c8256e78c7c631d8481109612f7cec6e47aeb
Pietro Borrelli <pietro.borrelli@outlook.com>
2019-01-13 16:57:03 +0100

    skipped pages with "properties.wr" name

--- src/main/java/com/engine/inspector/PageExtractorImpl.java
+++ src/main/java/com/engine/inspector/PageExtractorImpl.java
@@ -1,5 +1,6 @@
 package com.engine.inspector;
 
+import java.util.Arrays;
 
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
@@ -13,9 +14,11 @@ import org.w3c.dom.Node;
 public final class PageExtractorImpl implements PageExtractor {
 
 	private XPath xPath;
-	
+
+	private String[] blackListFiles = { "Properties.wr" };
+
 	public PageExtractorImpl() {
-		// create xPath to navigate the model 
+		// create xPath to navigate the model
 		XPathFactory xpathfactory = XPathFactory.newInstance();
 		this.setxPath(xpathfactory.newXPath());
 	}
@@ -23,40 +26,54 @@ public final class PageExtractorImpl implements PageExtractor {
 	/**
 	 * @param document
 	 * @return name of the page
-	 * @throws XPathExpressionException 
+	 * @throws XPathExpressionException
 	 */
-	public String extractPageName(Document document)  {
+	public String extractPageName(Document document) {
 		Node node = null;
+
+		// if page belongs to black list
+		if (Arrays.stream(blackListFiles)
+				.anyMatch(document.getBaseURI().substring(document.getBaseURI().lastIndexOf("/")+1)::equals))
+			return null;
+
 		try {
-		XPathExpression expr = getxPath().compile("/Page/@name");
-		node = (Node) expr.evaluate(document, XPathConstants.NODE);
-		}catch(XPathExpressionException xe) {
+			XPathExpression expr = getxPath().compile("/Page/@name");
+			node = (Node) expr.evaluate(document, XPathConstants.NODE);
+		} catch (XPathExpressionException xe) {
 			xe.printStackTrace();
 		}
-		if (node.getNodeValue() != null)
+		if (node != null && node.getNodeValue() != null)
 			return node.getNodeValue();
 		else
-			throw new NullPointerException("Attenzione: pagina con nome null. Documento di riferimento " + document.getDocumentURI());	
+			throw new NullPointerException(
+					"Attenzione: pagina con nome null. Documento di riferimento " + document.getDocumentURI());
 
 	}
-	
+
 	/**
 	 * @param document
 	 * @return name of the page
 	 * @throws XPathExpressionException
 	 */
-	public String extractPageId(Document document)  {
+	public String extractPageId(Document document) {
 		Node node = null;
+
+		// if page belongs to black list
+		if (Arrays.stream(blackListFiles)
+				.anyMatch(document.getBaseURI().substring(document.getBaseURI().lastIndexOf("/")+1)::equals))
+			return null;
+
 		try {
-		XPathExpression expr = getxPath().compile("/Page/@id");
-		node = (Node) expr.evaluate(document, XPathConstants.NODE);
-		}catch(XPathExpressionException xe) {
+			XPathExpression expr = getxPath().compile("/Page/@id");
+			node = (Node) expr.evaluate(document, XPathConstants.NODE);
+		} catch (XPathExpressionException xe) {
 			xe.printStackTrace();
 		}
 		if (node.getNodeValue() != null)
 			return node.getNodeValue();
 		else
-			throw new NullPointerException("Attenzione: pagina con id null. Documento di riferimento " + document.getDocumentURI());	
+			throw new NullPointerException(
+					"Attenzione: pagina con id null. Documento di riferimento " + document.getDocumentURI());
 
 	}
 
@@ -67,5 +84,6 @@ public final class PageExtractorImpl implements PageExtractor {
 	public void setxPath(XPath xPath) {
 		this.xPath = xPath;
 	}
+
 
 }
