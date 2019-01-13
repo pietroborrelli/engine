d61f451c73ea7ffd1006e18e4e8b90d7f3df041b
Pietro Borrelli <pietro.borrelli@outlook.com>
2019-01-13 02:48:37 +0100

    output for optimizations

--- src/main/java/com/engine/util/Output.java
+++ src/main/java/com/engine/util/Output.java
@@ -11,7 +11,6 @@ import com.engine.domain.abstractmodel.Collection;
 import com.engine.domain.abstractmodel.Entry;
 import com.engine.domain.interactionflowelement.InteractionFlowElement;
 
-
 @Component
 public class Output {
 
@@ -19,67 +18,86 @@ public class Output {
 	private String outputPathAbstractModel;
 	@Value("${output.path.physicalmodel}")
 	private String outputPathPhysicalModel;
-	
+	@Value("${output.path.abstractmodel.optimization}")
+	private String outputPathAbstractModelOptimization;
+	@Value("${output.path.physicalmodel.optimization}")
+	private String outputPathPhysicalModelOptimization;
+
 	/**
 	 * @param collection
 	 * @param path
 	 * @param area
 	 * @return void. Print in console NoSQL Abstract Model and the access path
 	 */
-	public void printAbstractModel(Collection collection, String area ) {
-		
-		System.out.println("Output on: " + outputPathAbstractModel + area + "/" +collection.getPath().getIdPath() + "/" + collection.getId() +"-"+ collection.getName()+".txt");
-		
-	    FileWriter fileWriter = null;
-		try {
-			fileWriter = new FileWriter(outputPathAbstractModel + area + "/"  + collection.getPath().getIdPath() + "/" + collection.getId() + "-"+ collection.getName()+".txt");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	    PrintWriter printWriter = new PrintWriter(fileWriter);
-	    //collection
-	    printWriter.println(collection.toString());
-	    //block
-	    printWriter.println("\n\tBlock " +
-	    	collection.getBlock().getKey().getPartitionKeys().toString() + " ; " + collection.getBlock().getKey().getSortKeys().toString());
-	    //entries
-	    printWriter.println("\n\t\tEntries");
-	    for (Entry entry : collection.getBlock().getEntries()) {
-	    	printWriter.println("\t\t\t" + entry.toString()); 
-	    }
+	public void printAbstractModel(Collection collection, String area, boolean optimization) {
 
-	    printWriter.println("\nPath:\n");
-	    	
-	    Integer count = 0;
-	    for (InteractionFlowElement ife : collection.getPath().getInteractionFlowElements()) {
-	    	count++;
-	    	printWriter.println(count + "." + ife.getClass() + "  ->  " + ife.getId() );
-	    }
-	    
-	    printWriter.close();
-	}
-	
-/**
- * @param collection
- * @param area
- * @param script
- * @return void. Print in consolle Physical Model -> script
- */
-public void printPhysicalModel(Collection collection,String area, String script) {
-		
-		System.out.println("Output on: " + outputPathPhysicalModel + area + "/" + collection.getPath().getIdPath() +"/"+ collection.getId() +"-"+ collection.getName()+".txt");
-		
-	    FileWriter fileWriter=null;
+		String outputPath = "";
+		if (optimization)
+			outputPath = outputPathAbstractModelOptimization + area + "/" + collection.getPath().getIdPath() + "/"
+			+ collection.getId() + "-" + collection.getName() + ".txt";
+		else
+			outputPath = outputPathAbstractModel + area + "/" + collection.getPath().getIdPath() + "/"
+					+ collection.getId() + "-" + collection.getName() + ".txt";
+			
+		System.out.println("Output on: " + outputPath);
+
+		FileWriter fileWriter = null;
 		try {
-			fileWriter = new FileWriter(outputPathPhysicalModel + area + "/" + collection.getPath().getIdPath() +"/"+  collection.getId() + "-"+ collection.getName()+".txt");
+			fileWriter = new FileWriter(outputPath);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	    PrintWriter printWriter = new PrintWriter(fileWriter);
-	    //script
-	    printWriter.println(script);
-	    
-	    printWriter.close();
+		PrintWriter printWriter = new PrintWriter(fileWriter);
+		// collection
+		printWriter.println(collection.toString());
+		// block
+		printWriter.println("\n\tBlock " + collection.getBlock().getKey().getPartitionKeys().toString() + " ; "
+				+ collection.getBlock().getKey().getSortKeys().toString());
+		// entries
+		printWriter.println("\n\t\tEntries");
+		for (Entry entry : collection.getBlock().getEntries()) {
+			printWriter.println("\t\t\t" + entry.toString());
+		}
+
+		printWriter.println("\nPath:\n");
+
+		Integer count = 0;
+		for (InteractionFlowElement ife : collection.getPath().getInteractionFlowElements()) {
+			count++;
+			printWriter.println(count + "." + ife.getClass() + "  ->  " + ife.getId());
+		}
+
+		printWriter.close();
+	}
+
+	/**
+	 * @param collection
+	 * @param area
+	 * @param script
+	 * @return void. Print in consolle Physical Model -> script
+	 */
+	public void printPhysicalModel(Collection collection, String area, String script, boolean optimization) {
+		String outputPath = "";
+		if (optimization)
+			outputPath = outputPathPhysicalModelOptimization + area + "/" + collection.getPath().getIdPath() + "/"
+					+ collection.getId() + "-" + collection.getName() + ".txt";
+		else
+			outputPath = outputPathPhysicalModel + area + "/" + collection.getPath().getIdPath() + "/"
+					+ collection.getId() + "-" + collection.getName() + ".txt";
+
+		System.out.println("Output on: " + outputPath);
+
+		FileWriter fileWriter = null;
+		try {
+			fileWriter = new FileWriter(outputPath);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		PrintWriter printWriter = new PrintWriter(fileWriter);
+		// script
+		printWriter.println(script);
+
+		printWriter.close();
 	}
 
 }
